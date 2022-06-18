74efa233971db439c90a3d57e717623aa5a4a5ad
rahulsinghparmar <rahulsinghparmar4@gmail.com>
2022-06-18 15:50:07 +0530

    added simple interface for management

---
+++ SimpleHospitalMS/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='HospitalManagementSystem',
+    version='2.7',
+    packages=['package'],
+    url='',
+    license='',
+    author='RahulSingh',
+    author_email='rahulsinghparmar4gmail.com',
+    description='Simple Hospital Management System Project'
+)
